23b9ce09cfb38435cadb6e8f186ffcfba8651dbf
Wei Mo <miloweimo@outlook.com>
2022-06-23 15:35:21 +0800

    啊啊
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

---
+++ src/views/HelloWDNMD.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="min-height: 200px;height: 100vh;background-color: aqua;">HelloWDNMD !!!</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWDNMD'
+}
+</script>
+
+<style>
+
+</style>
