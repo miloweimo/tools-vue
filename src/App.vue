5345379619aaad8fe4ad090377e9a49c303bc171
Wei Mo <miloweimo@outlook.com>
2022-06-23 21:07:08 +0800

    hide router view
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-    <nav>
+    <div v-if="this.$route.path === '/'" class="d-flex">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/wdnmd">wdnmd</router-link>
-    </nav>
+    </div>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
 
 <style>
 #app {
@@ -16,18 +22,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
