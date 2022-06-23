7c55d20006ac0bbddb6df62f7fe4ba0f79741850
Wei Mo <miloweimo@outlook.com>
2022-06-23 19:36:58 +0800

    add route
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

---
+++ src/components/basic/Button.js
@@ -0,0 +1,6 @@
+// styled-components.com
+const Button = () => (<button>Test</button>)
+
+const BlockButton = ({ value }) => (<button style="display: block">{value}</button>)
+
+export { Button, BlockButton }
