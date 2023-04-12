076ca9e54d6cc14521116d23e1139c2047177456
Wei Mo <miloweimo@outlook.com>
2023-04-12 14:17:42 +0800

    fix(store-foos):UI
    Signed-off-by: Wei Mo <miloweimo@outlook.com>

--- src/modules/store-food/components/ItemCard.vue
+++ src/modules/store-food/components/ItemCard.vue
@@ -27,29 +27,22 @@ export default {
 <style lang="scss">
 .item-card {
   min-height: 123px;
-  max-height: 160px;
   width: 100%;
   border: 1px solid #efe9c5;
   background-color: rgba($color: #efe9c5, $alpha: 0.5);
   display: flex;
   align-items: center;
   .item-card__img {
-    width: 15%;
-    height: 80%;
-    padding: 2px;
+    // width: calc(15%);
     img {
-      width: calc(90%);
+      padding: 2px;
+      height: 100px;
     }
   }
   .item-card__content {
     width: 100%;
     height: 100%;
     padding: 2px;
-    // display: flex;
-    // flex-direction: column;
-    // align-items: stretch;
-    // justify-content:flex-end;
-    // border: 1px solid;
     h3 {
       font-size: 1.2rem;
       font-weight: 600;
